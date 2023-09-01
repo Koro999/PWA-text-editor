2207ccc7ca25e0ff4b48600243fad7234ff5669f
Koro999 <chris.dang93@hotmail.com>
2023-08-31 21:58:01 -0400

    updated database logic, webpack compiling successfully

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,38 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('text-editor', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('text-editor')) {
+        console.log('text-editor database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('text-editor', { keyPath: 'id', autoIncrement: true });
+      console.log('text-editor database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const todosDb = await openDB('text-editor', 1);
+  const tx = todosDb.transaction('text-editor', 'readwrite');
+  const store = tx.objectStore('text-editor');
+  const request = store.put({ id: id, text: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+  };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const todosDb = await openDB('text-editor', 1);
+  const tx = todosDb.transaction('text-editor', 'readonly');
+  const store = tx.objectStore('text-editor');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
