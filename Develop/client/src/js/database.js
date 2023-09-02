306709765b3b2d3f6ea73332298306e7885c59f8
Koro999 <chris.dang93@hotmail.com>
2023-09-01 22:09:34 -0400

    fixed path for src-sw.js file. removed doubled up code, didnt see it in editor prior

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,4 +1,4 @@
-import { openDB } from 'idb';
+import { openDB} from 'idb';
 
 const initdb = async () =>
   openDB('text-editor', 1, {
@@ -15,10 +15,10 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
-  const todosDb = await openDB('text-editor', 1);
-  const tx = todosDb.transaction('text-editor', 'readwrite');
+  const db = await openDB('text-editor', 1);
+  const tx = db.transaction('text-editor', 'readwrite');
   const store = tx.objectStore('text-editor');
-  const request = store.put({ id: id, text: content });
+  const request = store.put({id: 1, text: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
   };
@@ -26,13 +26,13 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
-  const todosDb = await openDB('text-editor', 1);
-  const tx = todosDb.transaction('text-editor', 'readonly');
+  const db = await openDB('text-editor', 1);
+  const tx = db.transaction('text-editor', 'readonly');
   const store = tx.objectStore('text-editor');
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
